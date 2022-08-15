9eff03a3fe3f032ac6be7b611934feb28b0ad833
Caio Damasceno Alves <caio.damasceno@aluno.ufop.edu.br>
2022-08-15 18:35:55 -0300

    Recado pra gui

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();// instaciando variavel do express
 
 app.use((req, res, next) =>{
 	res.status(200).send({
-		mensagem: 'Ok, Deu certo !'
+		mensagem: 'Deu Certo GUIZINHO!'
 	});
 });//Consumindo metodo do express
 
