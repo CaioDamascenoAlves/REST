887f697cd38e0c4e8766faf15777bfd0fdeca5c8
Caio Damasceno Alves <caio.damasceno@aluno.ufop.edu.br>
2022-08-15 22:29:26 -0300

    INstalado morgan && desenvolvido tratativa de erro de rota

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const express = require('express');// importando express from
-const app = express();// instaciando variavel do express
+const app = express();// instanciando variavel do express
+const morgan = require('morgan');// instanciando variavel do morgan
 
 const rotaProdutos= require('./routes/produtos');// importando arquivo produtos na pasta router
 const rotaPedidos= require('./routes/pedidos');
 
+app.use(morgan('dev'));
+
 app.use('/produtos', rotaProdutos);// Usando rota produtos
 app.use('/pedidos', rotaPedidos);
+
+app.use((req,res, next) => {
+	const erro = new Error('Error Não encontrado');
+	erro.status = 404
+	next(erro);
+});
+
+app.use((error, req, res, next) => {
+	res.status(error.status || 500);
+	return res.send({
+		erro: {
+			mensagem: error.message
+		}
+	});
+});
 
 module.exports = app;
